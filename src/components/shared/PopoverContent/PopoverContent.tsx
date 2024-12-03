9f0c207181b50a6682ae418c54630a1ef968178a
Luka Vukovic <vukovicluka33@gmail.com>
2024-12-03 23:48:20 +0100

    Handle undefined quiz info values

--- src/components/shared/PopoverContent/PopoverContent.tsx
+++ src/components/shared/PopoverContent/PopoverContent.tsx
@@ -3,14 +3,16 @@ import { PopoverContent as Content, PopoverArrow, PopoverBody, PopoverTitle } fr
 import { Text } from "@chakra-ui/react";
 
 export default function PopoverContent({quiz}: {quiz: Quiz}) {
+  const created_at = new Date(quiz?.created_at || "").toLocaleDateString();
+  const updated_at = new Date(quiz?.updated_at || "").toLocaleDateString();
   return (
     <Content>
       <PopoverArrow />
       <PopoverBody>
         <PopoverTitle fontWeight="{fontWeights.bold}">Quiz Details</PopoverTitle>
-        <Text>Created: {new Date(quiz?.created_at || "").toLocaleDateString()}</Text>
-        <Text>Last updated: {new Date(quiz?.updated_at || "").toLocaleDateString()}</Text>
-        <Text>Plays: {quiz.plays}</Text>
+        <Text>Created: {created_at}</Text>
+        <Text>Last updated: {quiz.updated_at ? updated_at : created_at}</Text>
+        <Text>Plays: {quiz.plays || 0}</Text>
         <Text>Rating: { quiz.number_of_ratings ? quiz.average_rating + ` (${quiz.number_of_ratings})` : "not rated yet"}</Text>
       </PopoverBody>
     </Content>
